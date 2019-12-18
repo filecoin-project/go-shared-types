c7c7076b93b525e4caf7d53280ed7ee4dc8f39ee
laser <l@s3r.me>
2019-12-18 12:12:04 -0800

    move crypto into pkg

--- pkg/address/address_test.go
+++ pkg/address/address_test.go
@@ -15,7 +15,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"github.com/filecoin-project/go-shared-types/internal/crypto"
+	"github.com/filecoin-project/go-shared-types/pkg/crypto"
 )
 
 func init() {
